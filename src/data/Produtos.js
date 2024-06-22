063fadad70844d0bd8feb4c3f90e30710c59fa96
André Cunha <andrefigrox@gmail.com>
2024-06-22 14:30:17 -0300

    Add persistence

--- src/data/Produtos.js
+++ src/data/Produtos.js
@@ -1,7 +1,10 @@
-const produtos = []
+const produtos = localStorage.getItem('produtos')
+  ? JSON.parse(localStorage.getItem('produtos'))
+  : []
 
 export const addProduto = produto => {
   produtos.push(produto)
+  localStorage.setItem('produtos', JSON.stringify(produtos))
 }
 
 export const getProdutos = () => produtos
